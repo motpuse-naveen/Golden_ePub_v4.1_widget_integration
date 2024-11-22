3326f42608b64ba713d4bf540cc248b3be235e96
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2024-11-22 16:01:50 +0530

    Default ePub

---
+++ Golden_ePub_v4.1/OEBPS/widgets/Poptip/script/poptip.js
@@ -0,0 +1,9 @@
+var glossaries = {
+    l1: `<p><sup>*</sup> Banking that organizes large, complex financial transactions such as mergers or initial.</p>`,
+    
+    BIBu1: `<p>Morgon JP. <cite>Accelerated testing&#8212;statistical models, test plans, and data analyses</cite> Westminster, CO: Hobbs Engineering; 1990.</p>`,
+	BIB1: `<p>1. Morgon JP. <cite>Accelerated testing&#8212;statistical models, test plans, and data analyses</cite> Westminster, CO: Hobbs Engineering; 1990.</p>`,
+	bib_B1: `<p>Cyriax J. <cite>Textbook of orthopaedic medicine</cite>. In: <cite><i>Diagnosis of Soft Tissue Lesions</i></cite>. 8th ed. Vol 1. London: Balliere Tindall; 1982.</p>`,
+	bib_B2: `<p>Weed L. <cite>Medical records that guide and teach: part I</cite>. <cite><i>N Engl J Med</i></cite>. 1968;278:593-600.</p>`,
+
+}
